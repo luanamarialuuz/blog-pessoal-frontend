98d148e5c0bcd95a30c87d9659d041a2bf69a140
luanamariadaluzbarbosa@gmail.com <luanamariadaluzbarbosa@gmail.com>
2022-09-14 21:38:48 -0300

    Implementação de consumo HTTP - GET E POST

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'blogPessoalLuana'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blogPessoalLuana');
   });
 
   it('should render title', () => {
