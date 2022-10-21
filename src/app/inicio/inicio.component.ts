3c1d7f07c3a868c2d0c96cd8e9aca008bcc04cc9
luanamariadaluzbarbosa@gmail.com <luanamariadaluzbarbosa@gmail.com>
2022-10-20 23:22:27 -0300

    atualizando repositório

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
 import { Router } from '@angular/router';
+import { Postagem } from '../model/Postagem';
+import { PostagemService } from '../service/postagens.service';
+import { TemaService } from '../service/tema.service';
+import { AuthService } from '../service/auth.service';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +15,28 @@ import { Router } from '@angular/router';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+  tituloPost: string
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+  nomeTema: string
+
+  user: User = new User()
+  idUser = environment.id
+
+  key = 'data'
+  reverse = true
+
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    public authService: AuthService
+    
   ) { }
 
   ngOnInit(){
@@ -18,6 +44,30 @@ export class InicioComponent implements OnInit {
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
+
+    this.getAllTemas()
+  
   }
+
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+      this.tema = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.user.id = this.idUser
+    this.postagem.usuario = this.user
+  }
+  
 
 }
